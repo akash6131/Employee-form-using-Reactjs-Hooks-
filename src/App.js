8eef99e525780e3592889137282c3a2fc5b45240
akash6131 <aak59013@gmail.com>
2024-08-21 17:43:31 +0530

    First Commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import EmployeeForm from "./Employeeform.js";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Employee Form</h1>
+      <EmployeeForm/>
+    </div>
+  );
+}
+
+export default App;
